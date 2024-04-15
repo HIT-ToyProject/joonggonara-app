a3a5d055ec1bb14bb5a19c6ac3850ce6240ab68b
jaeseonNamgung <sunnamgung8@naver.com>
2024-04-15 16:01:31 +0800

    로그인 Entity 생성

---
+++ src/main/java/com/hit/joonggonara/entity/Member.java
@@ -0,0 +1,50 @@
+package com.hit.joonggonara.entity;
+
+import com.hit.joonggonara.type.LoginType;
+import com.hit.joonggonara.type.Role;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+@Entity
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    @Column(nullable = false)
+    private String name;
+    private String nickName;
+    private String password;
+    private String phoneNumber;
+    private String school;
+    @Column(nullable = false)
+    private LoginType loginType;
+    @Column(nullable = false)
+    private Role role;
+
+
+    @Builder
+    public Member(
+            String email,
+            String name,
+            String nickName,
+            String password,
+            String phoneNumber,
+            String school,
+            LoginType loginType,
+            Role role) {
+        this.email = email;
+        this.name = name;
+        this.nickName = nickName;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.school = school;
+        this.loginType = loginType;
+        this.role = role;
+    }
+}
