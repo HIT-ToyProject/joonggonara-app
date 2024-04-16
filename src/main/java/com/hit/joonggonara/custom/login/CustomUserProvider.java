1bfd150d6d172f17eee6e2257bafe23ce8682e12
jaeseonNamgung <sunnamgung8@naver.com>
2024-04-16 14:28:15 +0800

    주석 추가

--- src/main/java/com/hit/joonggonara/custom/login/CustomUserProvider.java
+++ src/main/java/com/hit/joonggonara/custom/login/CustomUserProvider.java
@@ -10,9 +10,11 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
 
 @RequiredArgsConstructor
+@Component
 public class CustomUserProvider implements AuthenticationProvider {
 
     private final CustomUserDetailsService userDetailsService;
@@ -21,8 +23,11 @@ public class CustomUserProvider implements AuthenticationProvider {
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String username = (String)authentication.getPrincipal();
         String password = (String)authentication.getCredentials();
+
+        // 데이터베이스에서 회원 정보를 꺼내옴
         UserDetails userDetails = userDetailsService.loadUserByUsername(username);
 
+        // 입력한 패스워드와 데이터베이스 안에 패스워드가 일치하는지 검사
         if(!passwordEncoder.matches(password, userDetails.getPassword())){
             throw new CustomException(UserErrorCode.USER_NOT_FOUND);
         }
